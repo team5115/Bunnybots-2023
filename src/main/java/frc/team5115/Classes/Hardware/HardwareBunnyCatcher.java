de74bff2bb8559c6527fffcaf72bf8f5197fa7e0
AlexGsilversat <alexg@silversat.org>
2023-12-08 15:24:48 -0500

    Pnumatics

--- src/main/java/frc/team5115/Classes/Hardware/HardwareBunnyCatcher.java
+++ src/main/java/frc/team5115/Classes/Hardware/HardwareBunnyCatcher.java
@@ -3,16 +3,28 @@ package frc.team5115.Classes.Hardware;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 public class HardwareBunnyCatcher {
     static final double positionConversionFactor = 360; // UNKNOWN
     final CANSparkMax roller;
     final RelativeEncoder encoder;
 
+    private DoubleSolenoid armSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 0);
+
     public HardwareBunnyCatcher() {
         roller = new CANSparkMax(5, MotorType.kBrushless);
         encoder = roller.getEncoder();
         encoder.setPositionConversionFactor(positionConversionFactor);
+    }
+
+    public void armIn(){
+        armSolenoid.set(Value.kReverse);
+    }
+
+    public void armOut(){
+        armSolenoid.set(Value.kForward);
     }
 
     public double getPosition() {
