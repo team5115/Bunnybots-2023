00bad819ec291532e5023825035f4d1a9ac61279
Jonathan <Idia_Ighile>
2023-12-16 13:10:32 -0500

    give me ur ohiooo

--- src/main/java/frc/team5115/Constants.java
+++ src/main/java/frc/team5115/Constants.java
@@ -76,9 +76,9 @@ public class Constants{
     public static final double kRotationalSlewRate = 2.0; // percent per second (1 = 100%)
 
     // Chassis configuration
-    public static final double kTrackWidth = Units.inchesToMeters(26.5);
+    public static final double kTrackWidth = Units.inchesToMeters(23);
     // Distance between centers of right and left wheels on robot
-    public static final double kWheelBase = Units.inchesToMeters(26.5);
+    public static final double kWheelBase = Units.inchesToMeters(23);
     // Distance between front and back wheels on robot
     public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
         new Translation2d(kWheelBase / 2, kTrackWidth / 2),
